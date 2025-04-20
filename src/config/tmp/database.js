d4978eb4d396dcfd54fbc71ffa5774703d7c5223
nhieelam <lam.nhiee11@gmail.com>
2025-04-20 08:25:58 +0700

    session

--- src/config/tmp/database.js
+++ src/config/tmp/database.js
@@ -2,6 +2,7 @@ const mysql=require('mysql2/promise')
 const pool=mysql.createPool({
     host:"localhost",
     user:"root",
+    port:3306,
     password:"kkkk",
     database:"cua_hang_sach"      
 })
